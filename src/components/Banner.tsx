e27c710da2343b81d473ba136c71d13e2a451020
dev <main.develop@protonmail.com>
2024-10-31 16:56:56 +0100

    Navbar added

---
+++ src/components/Banner.tsx
@@ -0,0 +1,16 @@
+export const Banner = () => {
+  return (
+    <div className="py-3 text-center gradient-background">
+      <div className="container">
+        <p className="font-medium text-opacity-50 text-white">
+          <span className="hidden sm:inline">
+            Convenient interface for visualizing Your data —{" "}
+          </span>
+          <a href="#" className="underline underline-offset-4">
+            Explore the demo.
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
