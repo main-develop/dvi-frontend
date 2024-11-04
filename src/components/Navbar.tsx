5ae533505de4204a8c31fba5602c662541bc16d8
dev <main.develop@protonmail.com>
2024-11-04 01:32:07 +0100

    Added login button animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,64 @@
+"use client";
 import Image from "next/image";
+import {
+  MotionValue,
+  ValueAnimationTransition,
+  animate,
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+} from "framer-motion";
+import { RefObject, useEffect, useRef } from "react";
+
 import logoImage from "@/assets/images/logo1.png";
 import MenuIcon from "@/assets/icons/menu.svg";
 
-export const Navbar = () => {
+const LoginButton = (): JSX.Element => {
+  const xPosition: MotionValue<number> = useMotionValue(0);
+  const yPosition: MotionValue<number> = useMotionValue(0);
+  const maskImage: MotionValue<string> = useMotionTemplate`radial-gradient(150px 150px at ${xPosition}% ${yPosition}%, black, transparent)`;
+  const divRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!divRef.current) return;
+
+    const { height, width } = divRef.current?.getBoundingClientRect();
+    const circumference: number = height * 2 + width * 2;
+
+    const times: number[] = [
+      0,
+      width / circumference,
+      (width + height) / circumference,
+      (width * 2 + height) / circumference,
+      1,
+    ];
+
+    const options: ValueAnimationTransition = {
+      duration: 4,
+      repeat: Infinity,
+      ease: "linear",
+      repeatType: "loop",
+      times: times,
+    };
+
+    animate(xPosition, [0, 100, 100, 0, 0], options);
+    animate(yPosition, [0, 0, 100, 100, 0], options);
+  }, []);
+
+  return (
+    <motion.div
+      ref={divRef}
+      style={{ maskImage: maskImage }}
+      className="inset-0 -m-px border border-[#00406C] rounded-lg"
+    >
+      <button className="py-2 px-4 bg-[#001B29]/50 text-white rounded-lg border border-white/15">
+        Log in
+      </button>
+    </motion.div>
+  );
+};
+
+export const Navbar = (): JSX.Element => {
   return (
     <header className="sticky top-0 backdrop-blur-sm shadow-lg z-50">
       <div className="px-4 py-4 flex justify-between items-center">
@@ -14,13 +70,15 @@ export const Navbar = () => {
             className="relative h-12 w-12"
           ></Image>
         </div>
-
         {/* For small screens */}
-        <div className="h-10 w-10 inline-flex justify-center items-center border border-white border-opacity-30 rounded-lg sm:hidden">
+        {/* <div className="h-10 w-10 inline-flex justify-center items-center border border-white border-opacity-30 rounded-lg sm:hidden">
           <MenuIcon className="text-white"></MenuIcon>
+        </div> */}
+        <div className="sm:hidden">
+          <LoginButton></LoginButton>
         </div>
-
         {/* For wide screens */}
+        {/* TODO: Use mapping instead */}
         <nav className="items-center gap-6 hidden sm:flex">
           <a
             href="#"
@@ -40,7 +98,7 @@ export const Navbar = () => {
           >
             Help
           </a>
-          <button className="py-2 px-4 bg-white rounded-lg">Log in</button>
+          <LoginButton></LoginButton>
         </nav>
       </div>
     </header>
