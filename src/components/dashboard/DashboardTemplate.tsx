7a7a1c1acb186fd53806437e7d67ceb229b327e9
dev <main.develop@protonmail.com>
2024-11-17 19:12:40 +0100

    Added navbar design

--- src/components/dashboard/DashboardTemplate.tsx
+++ src/components/dashboard/DashboardTemplate.tsx
@@ -4,11 +4,14 @@ import "./dashboard.css";
 import { motion } from "framer-motion";
 import { transition } from "@/utils/motions";
 import { Sidebar } from "./sidebar/Sidebar";
+import { Navbar } from "./navbar/Navbar";
 
 // TODO: Create Logo interface
 // TODO: Make correct display of ALL PAGES on md devices
 
-export const DashboardTemplate = (): React.JSX.Element => {
+export const DashboardTemplate = ({
+  children,
+}: Readonly<{ children: React.ReactNode }>): React.JSX.Element => {
   return (
     <motion.div
       initial="hidden"
@@ -17,6 +20,10 @@ export const DashboardTemplate = (): React.JSX.Element => {
       className="flex h-screen"
     >
       <Sidebar></Sidebar>
+      <div className="flex flex-col w-[86%]">
+        <Navbar></Navbar>
+        {children}
+      </div>
     </motion.div>
   );
 };
--- src/components/dashboard/sidebar/Sidebar.tsx
+++ src/components/dashboard/sidebar/Sidebar.tsx
@@ -2,29 +2,23 @@ import Link from "next/link";
 import Image from "next/image";
 import logoImage from "@/shared/assets/images/logo1.png";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
-import AccountAnimation from "@/shared/assets/animations/account-raw.json";
-import AssesmentAnimation from "@/shared/assets/animations/assesment-raw.json";
-import AvatarAnimation from "@/shared/assets/animations/avatar-raw.json";
-import BarChartAnimation from "@/shared/assets/animations/bar-chart-raw.json";
-import BinAnimation from "@/shared/assets/animations/bin-raw.json";
-import FileAnimation from "@/shared/assets/animations/file-raw.json";
-import HomeAnimation from "@/shared/assets/animations/home-raw.json";
-import PieChartAnimation from "@/shared/assets/animations/pie-chart-raw.json";
-import SearchAnimation from "@/shared/assets/animations/search.json";
-import SettingsAnimation from "@/shared/assets/animations/settings-raw.json";
+import AnalysisIcon from "@/shared/assets/animations/analysis.json";
+import FileIcon from "@/shared/assets/animations/file.json";
+import HomeIcon from "@/shared/assets/animations/home.json";
+import PieChartIcon from "@/shared/assets/animations/pie-chart.json";
 
 // TODO: Optimize list
 
 const navigationSections = [
-  { name: "Dashboard", href: "", icon: HomeAnimation },
-  { name: "Data sets", href: "", icon: FileAnimation },
-  { name: "Visualization", href: "", icon: PieChartAnimation },
-  { name: "AI Analysis", href: "", icon: BarChartAnimation },
+  { name: "Dashboard", href: "", icon: HomeIcon },
+  { name: "Data sets", href: "", icon: FileIcon },
+  { name: "Visualization", href: "", icon: PieChartIcon },
+  { name: "AI Analysis", href: "", icon: AnalysisIcon },
 ];
 
 export const Sidebar = (): React.JSX.Element => {
   return (
-    <div className="sidebar w-[14%] sm:w-[14%]">
+    <div className="sidebar relative w-[14%] sm:w-[14%] border-r-[2px]">
       <div className="flex items-center ml-2 mt-2">
         <Link href="/home">
           <div className="relative cursor-pointer flex-grow">
@@ -43,7 +37,7 @@ export const Sidebar = (): React.JSX.Element => {
       <div className="flex flex-col antialiased">
         <div className="flex-grow overflow-y-auto overflow-x-hidden">
           <ul className="flex flex-col space-y-1 py-8 px-1">
-            <li className="px-5">
+            <li className="px-4">
               <div className="flex flex-row items-center h-8">
                 <div className="hidden lg:block text-gray-500">Menu</div>
               </div>
@@ -54,17 +48,17 @@ export const Sidebar = (): React.JSX.Element => {
                   href={section.href}
                   className="relative flex flex-row items-center h-11 rounded-lg focus:outline-none hover:bg-[#0e0e0e] text-[#9ca3af] hover:text-[#c1c9d6]"
                 >
-                  <span className="inline-flex justify-center items-center ml-4">
+                  <span className="inline-flex justify-center items-center ml-3">
                     <DotLottieReact
                       data={section.icon}
                       playOnHover
-                      className="w-[30px] h-[30px]"
+                      className="w-[25px] h-[25px]"
                     ></DotLottieReact>
                   </span>
                   <span className="hidden lg:block ml-2 truncate">
                     {section.name}
                   </span>
-                  <span className="hidden lg:block px-2 py-0.5 ml-auto mr-[6px] text-xs font-medium tracking-wide text-[#06568c] bg-[#00416c3c] rounded-full">
+                  <span className="hidden lg:block px-2 py-0.5 ml-auto mr-[10px] text-xs font-medium tracking-wide text-[#06568c] bg-[#00416c3c] rounded-full">
                     15
                   </span>
                 </Link>
